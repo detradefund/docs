6003b76b3d157aa16d74412594dee0c52bb53100
detradefund <hello@detrade.fund>
2025-04-07 15:56:54 +0800

    Update Node version and Vercel adapter configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		port: 3002,
+		port: 3003,
 		strictPort: true // Empêche Vite d'utiliser un autre port si 3000 est occupé
 	},
 	build: {
