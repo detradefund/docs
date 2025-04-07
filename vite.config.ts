0919eb320cbfc0d386964d7f1fa8e4786bb940cf
detradefund <hello@detrade.fund>
2025-04-07 15:59:42 +0800

    Fix Vercel build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-		port: 3003,
-		strictPort: true // Empêche Vite d'utiliser un autre port si 3000 est occupé
+		port: 3000,
+		host: true
 	},
 	build: {
 		target: 'esnext'
 	}
-}); 
+});
